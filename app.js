3b33024cdcee1447112a2971115c16606ab41291
Das <aritdas@deloitte.com>
2018-03-12 12:24:05 +0530

    Change code to download zip file from server.

--- app.js
+++ app.js
@@ -116,9 +116,9 @@ app.get('/auth/sfdc', function(req, res) {
 
 app.get('/download', function(req, res) {
     console.log("ENT ERR! Permisson to access taarget.zip not granted. Provide folder permission 777.");
-    res.send("ENT ERR! Permisson to access taarget.zip not granted. Provide folder permission 777.");
+    //res.send("ENT ERR! Permisson to access taarget.zip not granted. Provide folder permission 777.");
     // response.download(process.cwd() + '/dist/target.zip');
-    //res.download(process.cwd() + '/dist/target.zip');
+    res.download(process.cwd() + '/dist/target.zip');
 })
 
 app.get('/oauth/_callback', function(req, res) {
